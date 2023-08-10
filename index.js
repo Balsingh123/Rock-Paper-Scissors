47f575acf18bf6d538c1341bcf79ed53c1bfdfb7
Balsingh123 <107838344+Balsingh123@users.noreply.github.com>
2023-08-10 11:26:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', () => {
+  userChoice = target.id
+  userChoiceDisplay.innerHTML = userChoice
+  generateComputerChoice()
+  getResult()
+}))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1 
+  
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.textContent = computerChoice
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = 'its a draw!'
+  }
+  else if (computerChoice === 'rock' && userChoice === "paper") {
+    result = 'you win!'
+  }
+  
+  else if (computerChoice === 'paper' && userChoice === "scissors") {
+    result = 'you win!'
+  }
+ 
+  else if (computerChoice === 'scissors' && userChoice === "rock") {
+    result = 'you win!'
+  }
+  else {
+    result = 'you lose!'
+  }
+  resultDisplay.textContent = result
+}
